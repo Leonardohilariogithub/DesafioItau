c43e5b5468c225d3bbc551a71e1af94d65c20f36
Leonardo <66699109+Leonardohilariogithub@users.noreply.github.com>
2022-03-02 16:50:11 -0300

    Add correçoes na configuração do mySQL dependencias e application.properties

--- src/main/java/com/desafioItau/entidades/OperacaoEntidade.java
+++ src/main/java/com/desafioItau/entidades/OperacaoEntidade.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +12,10 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "operacao")
 public class OperacaoEntidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private float saldo;
     private float extrato;
